b90d73b42710cddf14f5769dfb6b9695c5e01bb2
jingranwuxu <2234465260@qq.com>
2024-03-18 19:00:21 +0800

    修正

--- src/dividedbyzero/main.java
+++ src/dividedbyzero/main.java
@@ -2,11 +2,14 @@ package dividedbyzero;
 
 public class main {
 
-	/**
-	* @param args the command line arguments
-	*/
-	public static void main(String[] args) {
-	System.out.println(3/0);
-	}
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println(3/0);
+        } catch (ArithmeticException e) {
+            System.out.printf("Caught runtime exception = %s\n", e.getMessage());
+        }
+    }
 }
