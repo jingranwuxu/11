7bac1a429a5db02f4cf2494fbbfb7c721e5ca5f8
Paulliiii <2395951547@qq.com>
2024-03-19 14:51:21 +0800

    Exception specified

--- src/dividedbyzero/main.java
+++ src/dividedbyzero/main.java
@@ -7,10 +7,9 @@ public class main {
      */
     public static void main(String[] args) {
         try {
-            System.out.println(3 / 0);
-        } catch (Exception e) {
-            System.out.printf("Caught runtime exception: %s%n", e);
+            System.out.println(3/0);
+        } catch (ArithmeticException e) {
+            System.out.printf("Caught runtime exception = %s\n", e.getMessage());
         }
     }
-
 }
