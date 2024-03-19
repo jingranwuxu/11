7d0ef0c47ab8daf6878922a9b7021335eb8e4a33
jingranwuxu <2234465260@qq.com>
2024-03-19 12:20:25 +0800

    try/catch block added

--- src/dividedbyzero/main.java
+++ src/dividedbyzero/main.java
@@ -2,14 +2,11 @@ package dividedbyzero;
 
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        try {
-            System.out.println(3/0);
-        } catch (ArithmeticException e) {
-            System.out.printf("Caught runtime exception = %s\n", e.getMessage());
-        }
-    }
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+System.out.println(3/0);
+}
+
 }
