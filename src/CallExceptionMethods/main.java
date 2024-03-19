56f2324faf77b3fbf0a8b994707c7407ff73db36
jingranwuxu <2234465260@qq.com>
2024-03-19 15:31:38 +0800

    Exo2

---
+++ src/CallExceptionMethods/main.java
@@ -0,0 +1,16 @@
+package CallExceptionMethods;
+
+
+	public class main {
+	    public static void main(String[] args) {
+	        try {
+	            throw new Exception("My Exception");
+	        } catch (Exception e) {
+	            System.err.println("Caught Exception");
+	            // show error information with e.getMessage()
+	            System.err.println("getMessage(): " + e.getMessage());
+	            // show stack trace of the error with e.printStackTrace()
+	            e.printStackTrace();
+	        }
+	    }
+	}
